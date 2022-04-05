508c3c56d1335d2b063fc7dc5172c56e46b73dc0
osvald <80352026+Akapsio@users.noreply.github.com>
2022-04-05 23:42:30 +0300

    Update wordlist.py

--- digipeli/src/wordlist/wordlist.py
+++ digipeli/src/wordlist/wordlist.py
@@ -1,5 +1,9 @@
+import os
+
 path_to_names_of_wordlists = '/home/tee/Desktop/ot-harjoitustyo/digipeli/src/sanastojen_nimet.txt'
 path_to_test_wordlist = '/home/tee/Desktop/ot-harjoitustyo/digipeli/src/sanastot/testi_sanasto.txt'
+name_of_test_worldist = "testi_sanasto.txt"
+complete_name = os.path.join(path_to_test_wordlist, name_of_test_worldist)
 
 class Wordlist:
     
@@ -10,13 +14,20 @@ class Wordlist:
 
     def get_wordlist(self):
         return self.wordlist
+
+    def make_list_from_textfile(self):
+        with open(path_to_test_wordlist, "r") as filu:
+            textfile = filu.read()
+            wordfile = textfile.split(",")
+        for w in wordfile:
+            self.wordlist.append(w)
        
     def add_word(self, word):
         if len(self.wordlist) < self.length:
             self.wordlist.append(word)
             self.length += 1
         else:
-            print("Sanasto on täynnä")
+            raise ValueError("Sanasto on täynnä")
     
     def save_wordlist(self):
             with open(self.name, "w") as file:
@@ -24,5 +35,3 @@ class Wordlist:
                     file.write(f"{word}, ")
             with open(path_to_names_of_wordlists, "w") as wordlist:
                 wordlist.write(", {self.nimi}")
-	    
-
