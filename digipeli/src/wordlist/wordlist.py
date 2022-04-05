02e6a03383a317ac46978b6847ca27a31f51fabe
Teemu Pennanen <teemu.pennanen1@gmail.com>
2022-04-05 23:38:35 +0300

    Update

--- digipeli/src/wordlist/wordlist.py
+++ digipeli/src/wordlist/wordlist.py
@@ -1,41 +1,28 @@
-class Sanasto:
+path_to_names_of_wordlists = '/home/tee/Desktop/ot-harjoitustyo/digipeli/src/sanastojen_nimet.txt'
+path_to_test_wordlist = '/home/tee/Desktop/ot-harjoitustyo/digipeli/src/sanastot/testi_sanasto.txt'
+
+class Wordlist:
     
-    def __init__(self, nimi):
-        if self.tarkista_sanaston_nimen_saatavuus(nimi):
-            self.nimi = nimi
-        self.sanasto = []
-        self.pituus = 0
+    def __init__(self, name, length):
+        self.name = name
+        self.wordlist = []
+        self.length = length
 
-    def hae_sanasto(self, nimi):
-        try:
-            with open(nimi) as tiedosto:
-                sanasto = tiedosto.split(",")
-                return sanasto
-        except ValueError:
-            print(f"{nimi} nimistä tiedostoa ei löytynyt")
-
-    def tarkista_sanaston_nimen_saatavuus(self, nimi: str):
-        with open("sanastojen_nimet.txt") as tiedosto:
-            mjono = tiedosto.read()
-            sanastojen_nimet = mjono.split(",")
-            for sanasto in sanastojen_nimet:
-                if sanasto != nimi:
-                    return True
-                else:
-                    raise ValueError("Nimi on jo käytössä")
-        return False
-           
-    def lisaa_sana(self, sana):
-        if len(self.sanasto) < self.pituus:
-            self.sanasto.append(sana)
-            self.pituus += 1
+    def get_wordlist(self):
+        return self.wordlist
+       
+    def add_word(self, word):
+        if len(self.wordlist) < self.length:
+            self.wordlist.append(word)
+            self.length += 1
         else:
             raise ValueError("Sanasto on täynnä")
     
-    def tallenna_sanasto(self):
-            with open(self.nimi, "w") as tiedosto:
-                for sana in self.sanasto:
-                    tiedosto.write(f"{sana}, ")
-            with open("Sanastot") as tiedosto:
-                tiedosto.write(", {self.nimi}")
+    def save_wordlist(self):
+            with open(self.name, "w") as file:
+                for word in self.wordlist:
+                    file.write(f"{word}, ")
+            with open(path_to_names_of_wordlists, "w") as wordlist:
+                wordlist.write(", {self.nimi}")
 	    
+
