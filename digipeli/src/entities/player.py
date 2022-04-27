113e772944236a6d6cda53d6c5ba16b9d85c1556
Sami J. Laine <sami.j.laine@helsinki.fi>
2022-04-27 11:53:12 +0300

    Fixes for PlayerRepository to pass all four tests.

--- digipeli/src/entities/player.py
+++ digipeli/src/entities/player.py
@@ -1,12 +1,19 @@
 '''Unique players for the game'''
 class Player:
     '''Creates a new player with specified username and 0 victories'''
-    def __init__(self, username: str):
+    def __init__(self, username: str, victories = 0):
         self._username = username
-        self._victories = 0
+        self._victories = victories
 
     def get_name(self):
         return self._username
 
     def get_victories(self):
         return self._victories
+
+    def __str__(self):
+        return (
+            f'{{Player: '
+            f'[username={self._username}, '
+            f'victories={self._victories}]}}'
+        )
--- digipeli/src/tests/player_repository_test.py
+++ digipeli/src/tests/player_repository_test.py
@@ -2,26 +2,35 @@ import unittest
 import os
 from repositories.player_repository import PlayerRepository
 from entities.player import Player
+from tempfile import mkstemp
 
 dirname = os.path.dirname(__file__)
 
 class TestPlayerRepository(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestPlayerRepository, self).__init__(*args, **kwargs)
+        self.csv_path = os.path.join(mkstemp(prefix = 'digipeli',
+                                             suffix = '.csv')[1])
+
+
     '''Class for testing the Player class'''
     def setUp(self):
         '''Setting up a player repository'''
-        self.player_repository = PlayerRepository(os.path.join(dirname,
-        "../..", "data", "players-test.csv"))
+        self.player_repository = PlayerRepository(self.csv_path)
         self.player_repository.create(Player("default"))
-        
+
+    def tearDown(self):
+        os.remove(self.csv_path)
+
     def test_cannot_add_user_named_default(self):
         '''Test to see that you cannot add a username that is already in use'''
         self.assertRaises(ValueError, lambda: self.player_repository.create(Player("default")))
 
     def test_find_username_returns_True_when_looking_for_existing_player(self):
-        self.assertEqual(True, self.player_repository.find_username("default"))
+        self.assertTrue(self.player_repository.find_username("default"))
 
     def test_find_username_returns_False_when_looking_for_nonexisting_player(self):
-        self.assertEqual(False, self.player_repository.find_username("anonymous"))
+        self.assertFalse(self.player_repository.find_username("anonymous"))
 
     def test_find_all_returns_a_dictionary(self):
         self.assertEqual(type(self.player_repository.find_all()), dict)
