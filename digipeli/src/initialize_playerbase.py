b1aebc2247a48dddba6e4b5ff181166336b1685b
Teemu Pennanen <teemu.pennanen1@gmail.com>
2022-04-29 14:39:40 +0300

    yes

--- digipeli/src/initialize_playerbase.py
+++ digipeli/src/initialize_playerbase.py
@@ -6,18 +6,3 @@ dirname = os.path.dirname(__file__)
 
 player_repository = PlayerRepository(os.path.join(dirname, "..", "data", "players.csv"))
 
-player = Player("default")
-
-player_repository.create(player)
-player_repository.create(Player("Teemu"))
-player_repository.create(Player("Atte"))
-player_repository.create(Player("Riia"))
-player_repository.create(Player("Riku"))
-
-while True:
-    cmd = input("Pelaaja")
-    if cmd == "":
-        break
-    player_repository.create(Player(cmd))
-
-print(player_repository.find_all())
---
+++ digipeli/src/initialize_wordlists.py
@@ -0,0 +1,7 @@
+import os
+from repositories.wordlist_repository import WordlistRepository
+
+dirname = os.path.dirname(__file__)
+
+wordlist_repository = WordlistRepository(os.path.join(dirname, "..", "data",
+"wordlists.csv"))
